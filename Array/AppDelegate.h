d36bbfc09494a839033c42030a08f2176dcc975a
EnzoFurmenti <ducheenzo@mail.ru>
2016-07-21 18:58:46 +0300

    Initial Commit

---
+++ Array/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Array
+//
+//  Created by EnzoF on 21.07.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Array/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Array
+//
+//  Created by EnzoF on 21.07.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
